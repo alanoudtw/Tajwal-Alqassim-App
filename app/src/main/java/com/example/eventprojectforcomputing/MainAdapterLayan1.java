a08d6aca911f3467b60f02548ac14920feceedd7
LayanYousef <layanalsalloum@gmail.com>
2024-04-24 00:49:01 +0300

    updating

--- app/src/main/java/com/example/eventprojectforcomputing/MainAdapterLayan1.java
+++ app/src/main/java/com/example/eventprojectforcomputing/MainAdapterLayan1.java
@@ -1,10 +1,14 @@
 package com.example.eventprojectforcomputing;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -12,8 +16,14 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.FirebaseDatabase;
 import com.orhanobut.dialogplus.DialogPlus;
 import com.orhanobut.dialogplus.ViewHolder;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -30,7 +40,7 @@ public class MainAdapterLayan1 extends FirebaseRecyclerAdapter<MainModelLayan1,M
     }
 
     @Override
-    protected void onBindViewHolder(@NonNull myViewHolder holder, int position, @NonNull MainModelLayan1 model) {
+    protected void onBindViewHolder(@NonNull myViewHolder holder, final int position, @NonNull MainModelLayan1 model) {
         holder.eventname.setText(model.getEventname());
         holder.available.setText(model.getAvailable());
         holder.evaluation.setText(model.getEvaluation());
@@ -51,8 +61,81 @@ public class MainAdapterLayan1 extends FirebaseRecyclerAdapter<MainModelLayan1,M
                         .setExpanded(true,1800)
                         .create();
 
+                //dialogPlus1.show();
+
+                View view = dialogPlus1.getHolderView();
+
+                EditText eventname1 = view.findViewById(R.id.txteventname1);
+                EditText available1 = view.findViewById(R.id.txtavailable1);
+                EditText evaluation1 = view.findViewById(R.id.txtevaluation1);
+                EditText location1 = view.findViewById(R.id.txtlocation1);
+                EditText pictureurl1 = view.findViewById(R.id.txtpictureurl1);
+
+                Button btnUpdate = view.findViewById(R.id.btnupdate1);
+
+
+                eventname1.setText(model.getEventname());
+                available1.setText(model.getAvailable());
+                evaluation1.setText(model.getEvaluation());
+                location1.setText(model.getLocation());
+                pictureurl1.setText(model.getPicture());
+
                 dialogPlus1.show();
 
+                btnUpdate.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Map<String,Object> map = new HashMap<>();
+                        map.put("available",available1.getText().toString());
+                        map.put("evaluation",evaluation1.getText().toString());
+                        map.put("eventname",eventname1.getText().toString());
+                        map.put("location",location1.getText().toString());
+                        map.put("picture",pictureurl1.getText().toString());
+
+                        FirebaseDatabase.getInstance().getReference().child("Sport")
+                                .child(getRef(position).getKey()).updateChildren(map)
+                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void unused) {
+                                        Toast.makeText(holder.eventname.getContext(), "Updated Successfully", Toast.LENGTH_SHORT).show();
+                                        dialogPlus1.dismiss();
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(Exception e) {
+                                        Toast.makeText(holder.eventname.getContext(), "Oops, Updated Failed", Toast.LENGTH_SHORT).show();
+                                        dialogPlus1.dismiss();
+                                    }
+                                });
+                    }
+                });
+
+            }
+        });
+
+        holder.btnDelete1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder1 = new AlertDialog.Builder(holder.eventname.getContext());
+                builder1.setTitle("Are you sure you want to delete ?");
+                builder1.setMessage("Deleted data can not be UNDO");
+
+                builder1.setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        FirebaseDatabase.getInstance().getReference().child("Sport")
+                                .child(getRef(position).getKey()).removeValue();
+                    }
+                });
+
+                builder1.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(holder.eventname.getContext(), "Canceled", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                builder1.show();
             }
         });
 
